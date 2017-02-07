2c4ca4b1feb7c00da1bb79dd10cb2155c30554e1
davisliang <Davis@Daviss-MacBook-Pro.local>
2017-02-07 14:34:08 -0800

    changed piping an empty string to piping a double for consistency sake

--- cse221/4_5b.c
+++ cse221/4_5b.c
@@ -4,10 +4,10 @@
 #include <unistd.h>
 
 int fd[2];
-double begin, end;
+//double begin, end;
 void* thread_pipe(void* thread_nr){
-	
-	read(fd[0], "", sizeof(""));
+	double begin, end;
+	read(fd[0], &begin, sizeof(begin));
 	end = rdtsc();
 	printf("single context switch in kernel thread cost %f \n", end-begin);
 	close(fd[0]);
@@ -18,12 +18,13 @@ void* thread_pipe(void* thread_nr){
 int main(){
 	//int iter = 5;
 	int i=0;
-
+	double begin, end;
 	pipe(fd);
 	pthread_t first;
 	pthread_create(&first, NULL, thread_pipe, (void*)&i);
 	begin = rdtsc();
-	write(fd[1],"",sizeof(""));
+	//write(fd[1],"",sizeof(""));
+	write(fd[1],&begin,sizeof(begin));
 	pthread_exit(NULL);
 
 }
